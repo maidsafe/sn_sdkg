a433a8788a3693c52b358b4ebe8ff69a980b2f20
grumbach <anselmega@gmail.com>
2022-06-29 18:29:30 +0200

    chore: add missing LICENSE header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+// Copyright 2022 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
+// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
+// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. Please review the Licences for the specific language governing
+// permissions and limitations relating to use of the SAFE Network Software.
+//
+// The following code is based on hbbft : https://github.com/poanetwork/hbbft
+//
+// hbbft is copyright 2018, POA Networks, Ltd.
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. All files in the project
+// carrying such notice may not be copied, modified, or distributed except
+// according to those terms.
+
 mod sdkg;
 
 pub use sdkg::{to_pub_keys, AckOutcome, PartOutcome, SyncKeyGen};
