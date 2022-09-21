56ba9219638f597c579e86d31386dc2fecfa860f
grumbach <anselmega@gmail.com>
2022-09-21 21:21:04 +0200

    feat: docs and API test

--- README.md
+++ README.md
@@ -10,6 +10,16 @@ Synchronous Distributed Key Generation
 This Safe Network SDKG module enables sections to create a Section Key without a trusted dealer.
 It is based on the Audited code from poanetwork's [hbbft](https://github.com/poanetwork/hbbft).
 
+## How it works
+
+- Participants know of each other's `bls public key`
+- Each create a `Part` and share it with the others
+- They check each `Part` and share their `Ack` over each `Part`
+- Participants share their set of `AllAcks` signed, and check that all the others have the same set
+- Once everyone has all the participants signatures over this set they generate the key
+
+We differ from poanetwork's original implementation in that we require total participation: in the Part validation process, we require an Ack from everyone instead of just a threshold amount.
+
 ## Links
 
 - [hbbft keygen docs](https://docs.rs/hbbft/latest/hbbft/sync_key_gen/index.html)
--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,171 @@ pub use vote::{DkgSignedVote, NodeId};
 
 // For testing purposes
 pub use vote::DkgVote;
+
+// Test of sn_sdkg with explanations and usage example
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bls::{PublicKey, SecretKey, SignatureShare};
+    use std::collections::BTreeMap;
+
+    // fancy assert_match that returns inside content
+    // usage:
+    // `let data = assert_match(item_with_data, Enum::Pattern(data) => data)`
+    macro_rules! assert_match {
+       ($obj:expr, $pattern:pat $(if $pred:expr)* => $result:expr) => {
+           match $obj {
+               $pattern $(if $pred)* => $result,
+               _ => panic!("Assertion failed")
+           }
+       }
+    }
+
+    /// This test explains how sn_sdkg should be used
+    #[test]
+    fn test_dkg_for_the_rest_of_us() {
+        // Use the OS random number generator for any randomness:
+        let mut rng = bls::rand::rngs::OsRng;
+
+        // Generate individual key pairs for encryption. These are not suitable for threshold schemes.
+        let sec_key0: SecretKey = bls::rand::random();
+        let sec_key1: SecretKey = bls::rand::random();
+        let sec_key2: SecretKey = bls::rand::random();
+
+        let pub_keys: BTreeMap<u8, PublicKey> = BTreeMap::from([
+            (0, sec_key0.public_key()),
+            (1, sec_key1.public_key()),
+            (2, sec_key2.public_key()),
+        ]);
+
+        // Create a DkgState for each participants
+        let threshold = 1;
+        let mut dkg_state0 = DkgState::new(0, sec_key0, pub_keys.clone(), threshold, &mut rng)
+            .expect("Failed to create DKG state");
+        let mut dkg_state1 = DkgState::new(1, sec_key1, pub_keys.clone(), threshold, &mut rng)
+            .expect("Failed to create DKG state");
+        let mut dkg_state2 = DkgState::new(2, sec_key2, pub_keys, threshold, &mut rng)
+            .expect("Failed to create DKG state");
+
+        // Get the first votes: Parts
+        let part0 = dkg_state0.first_vote().expect("Failed to get first vote");
+        let part1 = dkg_state1.first_vote().expect("Failed to get first vote");
+        let part2 = dkg_state2.first_vote().expect("Failed to get first vote");
+
+        // Handle the other participants Parts, obtain Acks
+        // No need to handle our own vote
+        // Participant 0 handles Parts
+        // We already know this vote (it's ours), just checking that it gives IgnoringKnownVote
+        assert!(matches!(
+            dkg_state0.handle_signed_vote(part0.clone(), &mut rng),
+            Ok(VoteResponse::IgnoringKnownVote)
+        ));
+        assert!(matches!(
+            dkg_state0.handle_signed_vote(part1.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let acks0 = assert_match!(
+           dkg_state0.handle_signed_vote(part2.clone(), &mut rng),
+           Ok(VoteResponse::BroadcastVote(acks)) => *acks
+        );
+        // Participant 1 handles Parts
+        assert!(matches!(
+            dkg_state1.handle_signed_vote(part0.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let acks1 = assert_match!(
+            dkg_state1.handle_signed_vote(part2, &mut rng),
+            Ok(VoteResponse::BroadcastVote(acks)) => *acks
+        );
+        // Participant 2 handles Parts
+        assert!(matches!(
+            dkg_state2.handle_signed_vote(part0, &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let acks2 = assert_match!(
+            dkg_state2.handle_signed_vote(part1, &mut rng),
+            Ok(VoteResponse::BroadcastVote(acks)) => *acks
+        );
+
+        // Now that every participant handled the Parts and submitted their Acks, we handle the Acks
+        // Participant 0 handles Acks
+        assert!(matches!(
+            dkg_state0.handle_signed_vote(acks1.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let all_acks0 = assert_match!(
+           dkg_state0.handle_signed_vote(acks2.clone(), &mut rng),
+           Ok(VoteResponse::BroadcastVote(all_acks)) => *all_acks
+        );
+        // Participant 1 handles Acks
+        assert!(matches!(
+            dkg_state1.handle_signed_vote(acks0.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let all_acks1 = assert_match!(
+            dkg_state1.handle_signed_vote(acks2, &mut rng),
+            Ok(VoteResponse::BroadcastVote(all_acks)) => *all_acks
+        );
+        // Participant 2 handles Acks
+        assert!(matches!(
+            dkg_state2.handle_signed_vote(acks0, &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let all_acks2 = assert_match!(
+            dkg_state2.handle_signed_vote(acks1, &mut rng),
+            Ok(VoteResponse::BroadcastVote(all_acks)) => *all_acks
+        );
+
+        // Now that we have all the Acks, we check that everyone has the same set
+        // Handle the set of all acks to check everyone agreed on the same set
+        // Participant 0 handles AllAcks
+        assert!(matches!(
+            dkg_state0.handle_signed_vote(all_acks1.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let (pubs0, sec0) = assert_match!(
+           dkg_state0.handle_signed_vote(all_acks2.clone(), &mut rng),
+           Ok(VoteResponse::DkgComplete(pubs0, sec0)) => (pubs0, sec0)
+        );
+
+        // Participant 1 handles AllAcks
+        assert!(matches!(
+            dkg_state1.handle_signed_vote(all_acks0.clone(), &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let (pubs1, sec1) = assert_match!(
+            dkg_state1.handle_signed_vote(all_acks2, &mut rng),
+            Ok(VoteResponse::DkgComplete(pubs1, sec1)) => (pubs1, sec1)
+        );
+
+        // Participant 2 handles AllAcks
+        assert!(matches!(
+            dkg_state2.handle_signed_vote(all_acks0, &mut rng),
+            Ok(VoteResponse::WaitingForMoreVotes)
+        ));
+        let (pubs2, sec2) = assert_match!(
+            dkg_state2.handle_signed_vote(all_acks1, &mut rng),
+            Ok(VoteResponse::DkgComplete(pubs2, sec2)) => (pubs2, sec2)
+        );
+
+        // The pubkey sets should be identical
+        assert_eq!(pubs0, pubs1);
+        assert_eq!(pubs1, pubs2);
+
+        // Two sigs should be enough to sign a message
+        let msg = "signed message";
+        let sig_shares: BTreeMap<usize, SignatureShare> =
+            BTreeMap::from([(0, sec0.sign(msg)), (1, sec1.sign(msg))]);
+        let sig = pubs2
+            .combine_signatures(&sig_shares)
+            .expect("Failed to combine signatures");
+        assert!(pubs2.public_key().verify(&sig, msg));
+
+        let sig_shares: BTreeMap<usize, SignatureShare> =
+            BTreeMap::from([(1, sec1.sign(msg)), (2, sec2.sign(msg))]);
+        let sig = pubs0
+            .combine_signatures(&sig_shares)
+            .expect("Failed to combine signatures");
+        assert!(pubs0.public_key().verify(&sig, msg));
+    }
+}
